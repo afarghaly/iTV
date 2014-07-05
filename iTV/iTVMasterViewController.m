384d4dac52c12a49dbb826cdf605e6174bc44406
Ahmed Farghaly <afarghaly@gmail.com>
2014-07-04 20:29:52 -0700

    Added channel data and populated MasterViewController

--- iTV/iTVMasterViewController.m
+++ iTV/iTVMasterViewController.m
@@ -10,117 +10,136 @@
 
 #import "iTVDetailViewController.h"
 
-@interface iTVMasterViewController () {
-    NSMutableArray *_objects;
+@interface iTVMasterViewController ()
+{
+    NSArray *channelsData;
 }
+
 @end
+
+
+
+// - - - - - - - - - - - -
+
+
 
 @implementation iTVMasterViewController
 
+
+#pragma mark - UIViewController methods
+
+
+
 - (void)awakeFromNib
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+    {
         self.clearsSelectionOnViewWillAppear = NO;
         self.preferredContentSize = CGSizeMake(320.0, 600.0);
     }
+    
     [super awakeFromNib];
 }
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    
     self.detailViewController = (iTVDetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+    
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *channelsDataPath = [bundle pathForResource:@"channelsData" ofType:@"plist"];
+    channelsData = [NSArray arrayWithContentsOfFile:channelsDataPath];
 }
 
-- (void)didReceiveMemoryWarning
+
+- (void)viewDidLayoutSubviews
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidLayoutSubviews];
 }
 
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-}
 
-#pragma mark - Table View
+
+// - - - - - - - - - - - -
+
+
+
+#pragma mark - UITableViewDataSource methods
+
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
 }
 
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _objects.count;
+    return [channelsData count];
 }
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80.0f;
+}
+
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    NSString *cellID = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
 
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    if(nil == cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
+    }
+    
+    NSDictionary *channelData = channelsData[[indexPath row]];
+    
+    cell.textLabel.text = channelData[@"channelName"];
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:24];
+    
+    cell.detailTextLabel.text = channelData[@"channelCountry"];
+    cell.detailTextLabel.textColor = [UIColor grayColor];
+    
+    cell.imageView.image = [UIImage imageNamed:channelData[@"channelLogo"]];
+    
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        NSDate *object = _objects[indexPath.row];
-        self.detailViewController.detailItem = object;
+    NSLog(@"did select");
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+    {
+//        NSDate *object = _objects[indexPath.row];
+//        self.detailViewController.detailItem = object;
+        
+    }
+    else
+    {
+        
     }
 }
 
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+    if ([[segue identifier] isEqualToString:@"showDetail"])
+    {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
+//        [[segue destinationViewController] setDetailItem:object];
     }
 }
+
+
 
 @end
